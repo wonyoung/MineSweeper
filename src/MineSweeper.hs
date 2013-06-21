7355f519afa41c13719d3526c486b854e39336e3
Wonyoung <wonyoungjjang@gmail.com>
2013-06-21 17:33:50 +0900

    added mines which get number of near mines

--- src/MineSweeper.hs
+++ src/MineSweeper.hs
@@ -1,24 +1,38 @@
 module MineSweeper where
 import Data.Array.Repa
 
+-- *...
+-- ..*.
+-- ....
 
+-- *211
+-- 12*1
+-- 0111
+
+empty = '-'
+mine = '*'
 --inputs = repeat 0
-inputs = [1..]
+inputs = "*.....*....."
 
-board :: Int -> Int -> Array U DIM2 Int
+board :: Int -> Int -> Array U DIM2 Char
 board x y = fromListUnboxed (Z :. x :. y) $ take (x*y) inputs
 
-game = board 3 3
+game = board 4 3
 
-get :: Int -> Int -> Int
-get x y | x < 0 = 0
-        | y < 0 = 0
-        | x >= maxX = 0
-        | y >= maxY = 0
+get :: Int -> Int -> Char
+get x y | x < 0 = empty
+        | y < 0 = empty
+        | x >= maxX = empty
+        | y >= maxY = empty
         | otherwise = game ! (Z :. x :. y)
         where (Z :. maxY :. maxX) = extent game
 
-neighbors :: Int -> Int -> [Int]
+neighbors :: Int -> Int -> [Char]
 neighbors x y = [ get i j | i <- [x-1..x+1], j <-[y-1..y+1], i/=x || j/=y ]
 
-mines = length . filter (/= 0)
+mines :: Int -> Int -> Int
+mines x y = length . filter (== mine) $ neighbors x y
+
+
+
+
