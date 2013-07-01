99a62ad279997e255ab458e0647a427b2c48851d
Wonyoung <wonyoungjjang@gmail.com>
2013-07-01 11:07:16 +0900

    add openMine board x y

--- src/MineSweeper.hs
+++ src/MineSweeper.hs
@@ -1,5 +1,24 @@
 module MineSweeper where
-import Data.Array.Repa
+import Data.Array.Repa hiding ((++))
+
+data Board = Board { width :: Int
+                   , height :: Int
+                   , input :: [Char]
+                   }
+instance Show Board where
+        show (Board w h input) = show input
+        
+data Cell = Mine | NotCounted | MineCount Int | NoMines deriving (Read)
+        
+instance Show Cell where
+        show Mine = "*"
+        show (MineCount a) = show a
+        show NoMines = "-"
+        show NotCounted = "."
+
+readCell:: Char -> Cell
+readCell '*' = Mine
+readCell '.' = NotCounted
 
 -- *...
 -- ..*.
@@ -12,27 +31,40 @@ import Data.Array.Repa
 empty = '-'
 mine = '*'
 --inputs = repeat 0
-inputs = "*.....*....."
+mines = Board 4 3 "*.....*....."
 
-board :: Int -> Int -> Array U DIM2 Char
-board x y = fromListUnboxed (Z :. x :. y) $ take (x*y) inputs
+board :: Board -> Array U DIM2 Char
+board (Board x y inputs) = fromListUnboxed (Z :. y :. x) $ take (x*y) inputs
 
-game = board 4 3
-
-get :: Int -> Int -> Char
-get x y | x < 0 = empty
+get :: Board -> Int -> Int -> Char
+get b x y 
+        | x < 0 = empty
         | y < 0 = empty
-        | x >= maxX = empty
-        | y >= maxY = empty
-        | otherwise = game ! (Z :. x :. y)
-        where (Z :. maxY :. maxX) = extent game
+        | x >= w = empty
+        | y >= h = empty
+        | otherwise = (board b) ! (Z :. y :. x)
+        where (Board w h _) = b
 
-neighbors :: Int -> Int -> [Char]
-neighbors x y = [ get i j | i <- [x-1..x+1], j <-[y-1..y+1], i/=x || j/=y ]
+neighbors :: Board -> Int -> Int -> [Char]
+neighbors b x y = [ get b i j | i <- [x-1..x+1], j <-[y-1..y+1], i/=x || j/=y ]
 
-mines :: Int -> Int -> Int
-mines x y = length . filter (== mine) $ neighbors x y
+showMine :: Board -> Int -> Int -> Char
+showMine b x y 
+        | get b x y == mine = mine
+        | otherwise = head . show . length . filter (== mine) $ neighbors b x y
 
+countMines :: Board -> [Char]
+countMines b = [showMine b x y | y <-[0..h-1], x <- [0..w-1] ]
+        where (Board w h _) = b
 
+openAll :: Board -> Board
+openAll b = Board w h (countMines b)
+        where (Board w h _) = b
 
-
+open :: Board -> Int -> Int -> Board
+open b x y = Board w h (openMine b x y)
+        where (Board w h _) = b
+        
+openMine :: Board -> Int -> Int -> [Char]
+openMine b x1 y1 = [if x==x1 && y==y1 then showMine b x y else (last (take (x+y*w+1) m)) | y <-[0..h-1], x <- [0..w-1] ]
+        where (Board w h m) = b
